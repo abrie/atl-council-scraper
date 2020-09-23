cbb796968dce204277a9a941195f13d667235695
abrie <abrie@users.noreply.github.com>
2020-09-23 15:48:12 -0400

    Refactor: extract arugument parse into module.

---
+++ app/arguments.py
@@ -0,0 +1,17 @@
+import argparse
+import sys
+
+def parse(args):
+    parser = argparse.ArgumentParser(description="Scrape the city council website for council member information.")
+    subparser = parser.add_subparsers(dest="command", required=True)
+    parser_scrape = subparser.add_parser('scrape', description="Scrape the city council website for council member information.")
+    parser_scrape.add_argument('--out', dest="out", default=None, type=argparse.FileType('w', encoding='UTF-8'), help="save output here.")
+    group = parser_scrape.add_mutually_exclusive_group()
+    group.add_argument('--use-cache', dest='use_cache', type=argparse.FileType('r'), help='read HTML from this file.')
+    group.add_argument('--store-cache', dest='store_cache', type=argparse.FileType('w', encoding='UTF-8'), help='store HTML into this file')
+
+    if len(sys.argv)==1:
+        parser.print_help(sys.stderr)
+        sys.exit(1)
+
+    return parser.parse_args()

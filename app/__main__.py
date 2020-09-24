54effe8c37096c6c9e25cbba5a0b14f05c626302
abrie <abrie@users.noreply.github.com>
2020-09-23 20:58:56 -0400

    Trim first argv param (executable name) from arglist.

--- app/__main__.py
+++ app/__main__.py
@@ -5,7 +5,7 @@ import app.citycouncil.scraper
 import app.arguments
 
 if __name__ == '__main__':
-    args = app.arguments.parse(sys.argv)
+    args = app.arguments.parse(sys.argv[1:])
 
     commands = {
         "scrape": app.citycouncil.scraper.run,
